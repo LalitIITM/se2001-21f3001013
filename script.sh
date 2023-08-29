d4e07e49d770c19d291f2697e67773e84418da2c
Lalit <21f3001013@ds.study.iitm.ac.in>
2023-08-29 13:56:32 +0000

    added script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+vim script.sh
+---------------------------
+#!/bin/bash
+
+# Export the GitHub username
+export GITHUB_USERNAME="LalitIITM"
+
+# Export the GitHub repository name
+export GITHUB_REPOSITORY="se2001-21f3001013"
